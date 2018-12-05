0974743ea73bede2b1bf770d0abe210a61446c25
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2018-12-05 22:22:00 +0900

    add 0438

---
+++ Problems/0438_Find_All_Anagrams_in_a_String/Find_All_Anagrams_in_a_String.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public IList<int> FindAnagrams(string s, string p)
+    {
+        var n = p.Length;
+        var c = new int[26];
+        for (int i = 0; i < n; i++)
+            c[p[i] - 'a']++;
+
+        int v = n;
+        var kpath = new List<int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (c[s[i] - 'a'] > 0) v--;
+            c[s[i] - 'a']--;
+
+            if (i >= n)
+            {
+                c[s[i - n] - 'a']++;
+                if (c[s[i - n] - 'a'] > 0) v++;
+            }
+
+            if (v == 0)
+                kpath.Add(i - n + 1);
+        }
+        return kpath;
+    }
+
+    public IList<int> FindAnagrams2(string s, string p)
+    {
+        IList<int> resultArray = new List<int>();
+
+        if (s.Length < p.Length)
+            return resultArray;
+        
+        int[] p_char_count = new int[0x80];
+        int[] s_char_count = new int[0x80];
+
+        int i;
+        for (i = 0; i < p.Length; ++i) {
+            p_char_count[p[i]]++;
+        //  Console.WriteLine("p_char_count[" + p[i] + "] = " + p_char_count[p[i]]);
+        }
+        for (i = 0; i < p.Length - 1; ++i) {
+            s_char_count[s[i]]++;
+        //   Console.WriteLine("s_char_count[" + s[i] + "] = " + s_char_count[s[i]]);
+        }
+        for (i = p.Length - 1; i < s.Length; ++i)
+        {
+        //  Console.WriteLine("s_char_count[" + s[i] + "] = " + s_char_count[s[i]]);
+            s_char_count[s[i]]++;
+            if (i - p.Length >= 0)
+            {
+                s_char_count[s[i - p.Length]]--;
+            }
+            if (diff_char_count(s_char_count, p_char_count)) {
+                resultArray.Add(i - p.Length + 1);
+            }
+        }
+
+        return resultArray;
+    }
+
+    public bool diff_char_count(int[] p_char_count, int[] s_char_count)
+    {
+        for (int i = 0; i < p_char_count.Length; ++i) {
+            if (p_char_count[i] != s_char_count[i]) {
+            //  Console.WriteLine("p_char_count[" + Convert.ToChar(i) + "] = " + p_char_count[i] + ", s_char_count[" + Convert.ToChar(i) + "] = " + s_char_count[i]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string output_IList(IList<int> arr)
+    {
+        if (arr.Count <= 0)
+            return "[]";
+
+        string resultStr = "[" + arr[0];
+
+        for (int i = 1; i < arr.Count; ++i)
+            resultStr += "," + arr[i];
+
+        resultStr += "]";
+
+        return resultStr;
+    }
+
+    public void Main(string args)
+    {
+        string var_str = args.Replace("\"","").Replace("[","").Replace("]","").Trim();
+        string[] flds = var_str.Split(',');
+        string s = flds[0];
+        string p = flds[1];
+
+        Console.WriteLine("s = " + s + ", p = " + p);
+
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        IList<int> result = FindAnagrams(s, p);
+        Console.WriteLine("result = " + output_IList(result));
+        
+        sw.Stop();
+        Console.WriteLine("Execute time ... " + sw.ElapsedMilliseconds.ToString() + "ms");
+        /*
+        Console.Write("Hit Any Key");
+        Console.Read();
+        */
+    }
+}
---
+++ Problems/0438_Find_All_Anagrams_in_a_String/Find_All_Anagrams_in_a_String.py
@@ -0,0 +1,128 @@
+# coding: utf-8
+
+import os
+import sys
+import time
+import itertools
+
+class Solution:
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res = []
+        n, m = len(s), len(p)
+        if n < m:
+            return res
+        p_char_count, s_char_count = [0]*0x80, [0]*0x80
+        for x in p:
+            p_char_count[ord(x)] += 1
+        for x in s[:m-1]:
+            s_char_count[ord(x)] += 1
+        for i in range(m-1, n):
+            s_char_count[ord(s[i])] += 1
+            if i-m >= 0:
+                s_char_count[ord(s[i-m])] -= 1
+            if s_char_count == p_char_count:
+                res.append(i - m + 1)
+        return res
+
+
+    def findAnagrams_old(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        result = []
+        if len(s) < len(p):
+            return result
+        
+        if len(s) > 20100 or len(p) > 20100:
+            return result
+
+        turple_match_pattern = list(itertools.permutations(p, len(p)))
+    #   output_turple_match_pattern(turple_match_pattern)
+
+        match_pattern = []
+        for temp in turple_match_pattern:
+            match_pattern.append(turple_to_str(temp))
+    #   output_match_pattern(match_pattern)
+
+        start = 0
+        end = start + len(p)
+        while end <= len(s):
+            temp = s[start:end]
+            for target_pattern in match_pattern:
+                if temp == target_pattern:
+                    result.append(start)
+                    break
+            start += 1
+            end += 1
+
+        return result
+
+
+def turple_to_str(target):
+    resultStr = ""
+    for temp in target:
+        resultStr += temp
+    return resultStr
+
+
+def output_turple_match_pattern(turple_match_pattern):
+    for i in range(len(turple_match_pattern)):
+        print("turple_match_pattern[%d] = %s" %(i, turple_match_pattern[i]))
+
+
+def output_match_pattern(match_pattern):
+    for i in range(len(match_pattern)):
+        print("match_pattern[%d] = %s" %(i, match_pattern[i]))
+
+
+def main():
+    argv = sys.argv
+    argc = len(argv)
+
+    if (argc < 2):
+        print("Usage: python %s <testdata.txt>" %(argv[0]))
+        exit(0)
+
+    if not os.path.exists(argv[1]):
+        print("%s not found..." %argv[1])
+        exit(0)
+
+    testDataFile = open(argv[1], "r")
+    lines = testDataFile.readlines()
+
+    for temp in lines:
+        print("argv[1] = %s" %temp)
+        loop_main(temp)
+    #    print("Hit Return to continue...")
+    #    input()
+
+
+def loop_main(temp):
+    var_str = temp.replace("\"","").replace("[[","").replace("]]","").rstrip()
+    flds = var_str.split("],[")
+    s = flds[0]
+    p = flds[1]
+
+    print("s = %s, p = %s" %(s, p))
+
+    time0 = time.time()
+
+    sl = Solution()
+    result = sl.findAnagrams(s, p)
+
+    print("result = %s" %result)
+
+    time1 = time.time()
+    print("Execute time ... : %f[s]" %(time1 - time0))
+    print()
+
+
+if __name__ == "__main__":
+    main()
