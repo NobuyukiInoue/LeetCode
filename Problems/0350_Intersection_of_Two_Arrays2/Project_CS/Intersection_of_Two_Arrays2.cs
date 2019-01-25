3094acefbc3ce3eb76fdcd29690135ead82d5852
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2019-01-25 23:43:45 +0900

    add 0350

---
+++ Problems/0350_Intersection_of_Two_Arrays2/Project_CS/Intersection_of_Two_Arrays2.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+
+//Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+public class Solution
+{
+    public int[] Intersect(int[] nums1, int[] nums2)
+    {
+        if (nums1.Length <= 0 || nums2.Length <= 0)
+            return new int[] {};
+        
+        List<int> resultList = new List<int>();
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums1.Length; ++i)
+        {
+            if (!dic.ContainsKey(nums1[i]))
+                dic.Add(nums1[i], 1);
+            else
+                dic[nums1[i]]++;
+        }
+
+        for (int j = 0; j < nums2.Length; ++j)
+        {
+            if (dic.ContainsKey(nums2[j]))
+            {
+                resultList.Add(nums2[j]);
+                dic[nums2[j]]--;
+                if ((int)dic[nums2[j]] == 0)
+                    dic.Remove(nums2[j]);
+            }
+        }
+
+        return resultList.ToArray();
+    }
+
+    public int[] str_to_int_array(string s)
+    {
+        string[] flds = s.Split(',');
+        int[] nums = new int[flds.Length];
+
+        if (flds.Length <= 0)
+            return nums;
+
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            nums[i] = int.Parse(flds[i]);
+        }
+
+        return nums;
+    }
+
+    public string output_int_array(int[] nums)
+    {
+        if (nums.Length <= 0)
+            return "";
+
+        string resultStr = "[" +  nums[0].ToString();
+ 
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            resultStr += "," + nums[i].ToString();
+        }
+
+        return resultStr + "]";
+    }
+
+    public void Main(string args)
+    {
+        string[] nums_str = args.Trim().Replace("[[", "").Replace("]]", "").Split(new string[] {"],["}, StringSplitOptions.None);
+        int[] nums1 = str_to_int_array(nums_str[0]);
+        int[] nums2 = str_to_int_array(nums_str[1]);
+        Console.WriteLine("nums1 = " + output_int_array(nums1));
+        Console.WriteLine("nums2 = " + output_int_array(nums2));
+
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        int[] result = Intersect(nums1, nums2);
+        Console.WriteLine("result = " + output_int_array(result));
+        
+        sw.Stop();
+        Console.WriteLine("Execute time ... " + sw.ElapsedMilliseconds.ToString() + "ms\n");
+    }
+}
---
+++ Problems/0350_Intersection_of_Two_Arrays2/Project_Python3/Intersection_of_Two_Arrays2.py
@@ -0,0 +1,88 @@
+from collections import Counter
+import os
+import sys
+import time
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        c1, c2 = Counter(nums1), Counter(nums2)
+        return sum([[num] * min(c1[num], c2[num]) for num in c1 & c2], [])
+
+    def intersect2(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        d = {}
+        result = []
+        
+        for num in nums1:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+        
+        for num in nums2:
+            if num in d:
+                if d[num] > 0:
+                    d[num] -= 1
+                    result.append(num)
+                    
+        return result
+
+def str_to_int_array(flds):
+    if len(flds) <= 0:
+        return None
+    temp = flds.split(",")
+    nums = [0]*len(temp)
+    for i in range(len(temp)):
+        nums[i] = int(temp[i])
+    return nums
+
+def main():
+    argv = sys.argv
+    argc = len(argv)
+
+    if (argc < 2):
+        print("Usage: python %s <testdata.txt>" %(argv[0]))
+        exit(0)
+
+    if not os.path.exists(argv[1]):
+        print("%s not found..." %argv[1])
+        exit(0)
+
+    testDataFile = open(argv[1], "r")
+    lines = testDataFile.readlines()
+
+    for temp in lines:
+        print("argv[1] = %s" %temp)
+        loop_main(temp)
+    #    print("Hit Return to continue...")
+    #    input()
+
+
+def loop_main(temp):
+    nums_str = temp.rstrip().replace("[[", "").replace("]]", "").split("],[")
+    nums1 = str_to_int_array(nums_str[0])
+    nums2 = str_to_int_array(nums_str[1])
+
+    time0 = time.time()
+
+    sl = Solution()
+    result = sl.intersect(nums1, nums2)
+
+    time1 = time.time()
+
+    print("result = %s" %result)
+
+    print("Execute time ... : %f[s]" %(time1 - time0))
+    print()
+
+if __name__ == "__main__":
+    main()
